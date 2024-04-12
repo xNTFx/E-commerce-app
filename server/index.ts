a0421c4666c9e9cabc96e69db011183bc45ffea4
/xNTFx <ppawel1388@gmail.com>
2024-04-12 21:16:24 +0200

    24th commit

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,29 @@ import express from "express";
 import cors from "cors";
 import setupPaymentRoutes from "./payment.js";
 import mongoDB from "./mongoDBQueries.js";
+import helmet from "helmet";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'https://shopping-page-client-cn8a6er1n-ntfvs-projects.vercel.app'
-  }));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "https://js.stripe.com"],
+        connectSrc: ["'self'", "https://api.example.com"],
+        imgSrc: ["'self'", "data:"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+      },
+    },
+  })
+);
+
+app.use(
+  cors({
+    origin: "https://shopping-page-client-cn8a6er1n-ntfvs-projects.vercel.app",
+  })
+);
 mongoDB(app);
 setupPaymentRoutes(app);
 
