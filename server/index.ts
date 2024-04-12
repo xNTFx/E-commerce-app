de3a6910180df0109f9bf9382515e8fcaf6fc9fd
/xNTFx <ppawel1388@gmail.com>
2024-04-12 23:43:57 +0200

    38th commit

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import setupPaymentRoutes from "./payment.js";
@@ -38,6 +40,9 @@ setupPaymentRoutes(app);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+  res.send("user:" + process.env.DB_USERNAME);
+  res.send("pass: " + process.env.DB_PASSWORD);
+  res.send("cluster:" + process.env.DB_CLUSTERURL);
 });
 
 const PORT = process.env.PORT || 3000;
