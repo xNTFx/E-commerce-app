041d2901e064416db6f66ab303c474767d66f451
/xNTFx <ppawel1388@gmail.com>
2024-04-12 21:53:29 +0200

    27th commit

--- server/index.ts
+++ server/index.ts
@@ -16,6 +16,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+    res.header({"Access-Control-Allow-Origin": "*"});
+    next();
+  }) 
+
 app.use(
   helmet({
     contentSecurityPolicy: {
