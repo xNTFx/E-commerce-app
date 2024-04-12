3b90e584178064f446c472dbc30f388622653701
/xNTFx <ppawel1388@gmail.com>
2024-04-13 00:47:52 +0200

    47th commit

--- server/api/index.ts
+++ server/api/index.ts
@@ -8,16 +8,14 @@ import helmet from "helmet";
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://shopping-page-client.vercel.app",
-  methods: "GET, POST, PUT, DELETE, OPTIONS",
-  allowedHeaders: "Content-Type, Authorization",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 // app.use(
 //   helmet({
@@ -32,8 +30,6 @@ app.use(cors(corsOptions));
 //     },
 //   })
 // );
-
-app.options("*", cors(corsOptions));
 
 mongoDB(app);
 setupPaymentRoutes(app);
