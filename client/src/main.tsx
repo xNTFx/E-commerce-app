cc925679f3f0a90c6ad7af1060d85e53e983ae12
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2024-04-24 04:46:59 +0200

    Update main.tsx v2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './css/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
