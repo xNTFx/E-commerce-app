b2e10be3e52c581eace1697a61d7189c992c4035
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2024-04-24 04:45:03 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './css/index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './css/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
