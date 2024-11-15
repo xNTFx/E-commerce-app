db1b0ff24a3eaa77c57b5f402febf99ac7f0f211
Paweł Soboń <ppawel1388@gmail.com>
2024-11-15 08:35:30 +0100

    Removed react strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app/App';
@@ -12,8 +11,4 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
